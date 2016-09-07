a4f84441cbb0624a815be62d7e26c225fe22e345
Josef Šimánek <josef.simanek@gmail.com>
2016-09-07 23:12:25 +0200

    Use logger only in dev env.

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
 app.use(favicon(__dirname + '/public/img/favicon.ico'));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -53,10 +52,12 @@ app.use(function(req, res, next) {
 
 /// error handlers
 
-// development error handler
+// development error handler and logger
 // will print stacktrace
 
 if (app.get('env') === 'development') {
+    app.use(logger('dev'));
+
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
