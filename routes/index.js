fd243d8c2f083b8508b37a80a5e1f8abf33b79cc
Pavel Ševčík <pajaseviwow@gmail.com>
2019-05-05 17:29:39 +0200

    refactored processing method

--- routes/index.js
+++ routes/index.js
@@ -1,50 +1,41 @@
 const express = require('express');
+const assert = require('assert');
 const router = express.Router();
 const uncss = require('uncss');
-const fs = require('fs');
 const multer  = require('multer');
 const upload = multer();
-const uuid = require('node-uuid');
 
 /* GET home page. */
 
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.render('index', {
     title: 'UnCSS Online!',
     description: 'Simply UnCSS your styles online!'
   });
 });
 
-router.get('/uncss', function(req, res) {
-  res.redirect('/');
-});
+router.post('/api/uncss', upload.array(), (req, res) => {
+  try {
+    assert.ok(req.body.inputHtml, new Error('cannot process empty HTML'));
+    assert.ok(req.body.inputCss, new Error('cannot process empty CSS'));
 
-router.post('/uncss', upload.array(), function(req, res) {
-  uncss(req.body.inputHtml, {
-    raw: req.body.inputCss,
-    javascriptEnabled: false,
-    banner: false
-  }, function(error, output) {
-    if (req.body.type === 'fetch') {
+    uncss(req.body.inputHtml, {
+      raw: req.body.inputCss,
+      javascriptEnabled: false,
+      banner: false
+    }, (error, output) => {
       res.json({
         outputCss: output,
         error: error
       });
-    }
-    else {
-      res.render('index', {
-        title: 'UnCSS Online!',
-        description: 'Simply UnCSS your styles online!',
-        inputHtml: req.body.inputHtml,
-        inputCss: req.body.inputCss,
-        outputCss: output,
-        error: {
-          name: error.name,
-          message: error.reason + '; Line:' + (error.line -1) + '; Column:' + error.column
-        }
-      });
-    }
-  });
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.json({
+      error: error
+    });
+  }
 });
 
 module.exports = router;
