44ea5ac6a8b348da4b517ff772459f50bd8bb7d6
Pavel Ševčík <pajaseviwow@gmail.com>
2019-05-24 14:09:51 +0200

    serialize uncss error

--- api/uncss/index.js
+++ api/uncss/index.js
@@ -26,7 +26,7 @@ module.exports = async (req, res) => {
 
         send(res, error ? 400 : 200, {
           outputCss: output,
-          error: error
+          error: serializeError(error)
         });
       }
     );
