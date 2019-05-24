6e0c02b6b096941fb33fb0828bf947de87855cd5
Pavel Ševčík <pajaseviwow@gmail.com>
2019-05-24 13:01:13 +0200

    added sentry to BE

--- api/uncss/index.js
+++ api/uncss/index.js
@@ -1,6 +1,9 @@
 const { send, json } = require("micro");
 const uncss = require("uncss");
 const serializeError = require("serialize-error");
+const Sentry = require('@sentry/node');
+
+Sentry.init({ dsn: process.env.SENTRY_DSN });
 
 module.exports = async (req, res) => {
   const data = await json(req, { limit: "14mb" });
@@ -19,6 +22,8 @@ module.exports = async (req, res) => {
         ignoreSheets: [/./]
       },
       (error, output) => {
+        Sentry.captureException(error);
+
         send(res, error ? 400 : 200, {
           outputCss: output,
           error: error
@@ -26,6 +31,7 @@ module.exports = async (req, res) => {
       }
     );
   } catch (error) {
+    Sentry.captureException(error);
     console.error(error);
 
     send(res, 400, {
