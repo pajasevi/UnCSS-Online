62a2111ea90abe36a4935849b6c69d6cae10e9f9
Pavel Ševčík <pajaseviwow@gmail.com>
2019-07-08 14:51:58 +0200

    Removed unnecessary getInitialProps in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,16 +8,6 @@ import "milligram/dist/milligram.min.css";
 import "../main.css";
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   render() {
     const { Component, pageProps } = this.props;
 
