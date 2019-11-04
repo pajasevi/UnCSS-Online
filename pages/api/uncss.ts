e0b94806de65140a5c8bd3ade85c760550cc14e4
Pavel Ševčík <pajaseviwow@gmail.com>
2019-11-04 22:59:42 +0100

    Updated dependencies

--- pages/api/uncss.ts
+++ pages/api/uncss.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import uncss from "uncss";
-import serializeError from "serialize-error";
+import { serializeError } from "serialize-error";
 import * as Sentry from '@sentry/node';
 
 Sentry.init({ dsn: process.env.SENTRY_BE_DSN });
