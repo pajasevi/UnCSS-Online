10f6bf1dab5df93f491d03f87ad7cac4aa59a4dc
Pavel Ševčík <pavel.sevcik@protonmail.com>
2023-11-15 05:38:34 +0100

    Updated dependencies

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  chromeWebSecurity: false,
+  video: false,
+
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+};
